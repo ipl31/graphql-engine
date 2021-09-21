0d7be844a02fb92128604eb9bc769b6a73d0be5c
Ken Caruso <ken@ipl31.net>
2021-09-20 21:39:44 -0700

    Update get.sh
    Remove broken bash

--- cli/get.sh
+++ cli/get.sh
@@ -151,9 +151,3 @@ if [ -e $targetFile ]; then
 fi
 
 hasura version
-
-if ! $(echo "$PATH" | grep -q "$INSTALL_PATH"); then
-    log
-    log "${YELLOW}$INSTALL_PATH not found in \$PATH, you might need to add it${NC}"
-    log 
-fi
